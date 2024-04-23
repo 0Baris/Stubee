05054a4642174da3fdac58423a5b1f64331fcc8c
Barış Cem Ant <baris.ant@ogr.gelisim.edu.tr>
2024-04-23 20:59:36 +0300

    To-Doo Pop-Up Design
    Signed-off-by: Barış Cem Ant <baris.ant@ogr.gelisim.edu.tr>

--- app/src/main/java/com/example/stubee/ToDooEkle.java
+++ app/src/main/java/com/example/stubee/ToDooEkle.java
@@ -1,20 +1,40 @@
 package com.example.stubee;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 
 public class ToDooEkle extends Fragment {
 
+    Calendar gününtakvimi = Calendar.getInstance();
+    private int takYıl = gününtakvimi.get(Calendar.YEAR);
+    private int takAy = gününtakvimi.get(Calendar.MONTH);
+    private int takGun = gününtakvimi.get(Calendar.DAY_OF_MONTH);
+    CardView todoo_cardview;
+    TextView tarihyazi;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        View view4 = inflater.inflate(R.layout.fragment_todooekle, container, false);
+
+
+
         return inflater.inflate(R.layout.fragment_todooekle, container, false);
     }
+
+
 }
--- app/src/main/java/com/example/stubee/ToDooFragment.java
+++ app/src/main/java/com/example/stubee/ToDooFragment.java
@@ -1,18 +1,20 @@
 package com.example.stubee;
 
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 
-import androidx.appcompat.widget.AppCompatButton;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
-import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -20,7 +22,12 @@ public class ToDooFragment extends Fragment {
 
     FloatingActionButton todoo_button;
     CardView todoo_cardview;
-
+    TextView tarihyazi;
+    boolean todoo_durum = false;
+    Calendar gününtakvimi = Calendar.getInstance();
+    private int takyil = gününtakvimi.get(Calendar.YEAR);
+    private int takAy = gününtakvimi.get(Calendar.MONTH);
+    private int takGun = gününtakvimi.get(Calendar.DAY_OF_MONTH);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -46,32 +53,57 @@ public class ToDooFragment extends Fragment {
     public void popUpGoster(View anchorView){
         LayoutInflater layoutInflater = LayoutInflater.from(getContext());
         View popupView = layoutInflater.inflate(R.layout.fragment_todooekle, null);
-
+        todoo_durum = false;
         PopupWindow todoo_popup = new PopupWindow(popupView, ViewGroup.LayoutParams.MATCH_PARENT , ViewGroup.LayoutParams.WRAP_CONTENT);
-
-        todoo_popup.showAtLocation(anchorView, Gravity.CENTER,0,0);
         todoo_popup.setFocusable(true);
 
-        Button to_kapat = popupView.findViewById(R.id.todoo_kapatbutton);
+        if  (todoo_durum == false){
+            todoo_durum = true;
 
-        to_kapat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                todoo_popup.dismiss();
-            }
-        });
+            todoo_popup.showAtLocation(anchorView, Gravity.CENTER,0,0);
+
+            TextView tarihtext = popupView.findViewById(R.id.todoo_tarihtext);
+            tarihtext.setText(takGun+"."+takAy+"."+ takyil);
+
+            Button tarihb = popupView.findViewById(R.id.todoo_tarihbutton);
+            tarihb.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),R.style.DialogTheme, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int yil, int ay, int gun) {
+
+                            tarihtext.setText(String.valueOf(gun)+"."+String.valueOf(ay)+"."+String.valueOf(yil));
+
+                        }
+                    }, takyil, takAy, takGun);
+
+                    datePickerDialog.show();
+                }
+            });
 
 
-        Button to_kaydet = popupView.findViewById(R.id.todoo_kaydetbutton);
-        to_kaydet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                todoo_popup.dismiss();
-            }
-        });
+            Button to_kapat = popupView.findViewById(R.id.todoo_kapatbutton);
+            to_kapat.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    todoo_durum = false;
+                    todoo_popup.dismiss();
+                }
+            });
+
+
+            Button to_kaydet = popupView.findViewById(R.id.todoo_kaydetbutton);
+            to_kaydet.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    todoo_durum = false;
+
+                }
+            });
+
+        }
 
     }
-
-
 
 }
